779c53b95719a0ed9a04999ac360b7cc4368c16c
CrimsonVoid <CrimsonVoid@users.noreply.github.com>
2014-10-14 16:11:55 -0500

    Remove 'wbr'

--- modules/url/vars.go
+++ modules/url/vars.go
@@ -13,7 +13,7 @@ const (
 
 var (
 	htmlCleanerR = regexp.MustCompile(fmt.Sprintf(`</?[%v].*?>`,
-		`a|br|code|span`,
+		`a|br|code|span|wbr`,
 	))
 
 	urlRe = regexp.MustCompile(`(http|https)\://[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(:[a-zA-Z0-9]*)?/?([a-zA-Z0-9\-\._\?\,\'/\\\+&amp;%\$#\=~])*`)
